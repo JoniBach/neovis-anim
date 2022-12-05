e4c420d09a8fd085569fedcd80cb8a74f0ce56e8
James Crook <48724197+JoniBach@users.noreply.github.com>
2022-12-05 17:27:54 +0000

    ci cd test

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,8 +7,6 @@ import { Bar } from "../components/Bar";
 import { Input } from "../components/Input";
 import { Splash } from "../components/Splash";
 
-// branch rules test 2
-
 export const Signup = () => {
   const goto = useNavigate();
   const [error, setError] = useState("");
